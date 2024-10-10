1d0e2006a4272d2f15cf5778c2f5c76209d21e0a
Michael Chandra <michaelchandra700@gmail.com>
2024-10-11 02:30:14 +0700

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+const hm = document.querySelector("#hamburger-menu");
+// const link = document.querySelector(".navbar-nav a");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
